fcbe616ac4c07bc65b6d0c2884caa4352e7d2b9d
Victor Turansky <victor.turansky@gmail.com>
2021-06-20 17:51:03 +0300

    Simplify `UserCard` declaration

--- src/main/kotlin/ringui/UserCard.kt
+++ src/main/kotlin/ringui/UserCard.kt
@@ -1,14 +1,11 @@
+@file:JsModule("@jetbrains/ring-ui/components/user-card/user-card")
+
 package ringui
 
-import react.RBuilder
 import react.RClass
-import react.RHandler
 import react.RProps
 
-@JsModule("@jetbrains/ring-ui/components/user-card/user-card")
-private external object UserCardModule {
-    val UserCard: RClass<UserCardProps>
-}
+public external val UserCard: RClass<UserCardProps>
 
 // https://github.com/JetBrains/ring-ui/blob/master/components/user-card/card.js
 public external interface UserCardProps : RProps {
@@ -16,23 +13,16 @@ public external interface UserCardProps : RProps {
     public var wording: UserCardWording
 }
 
-public data class UserCardModel(
-    val name: String,
-    val login: String,
-    val avatarUrl: String,
-    val email: String? = null,
-    val href: String? = null
-)
+public external interface UserCardModel {
+    public val name: String
+    public val login: String
+    public val avatarUrl: String
+    public val email: String?
+    public val href: String?
+}
 
-public data class UserCardWording(
-    val banned: String,
-    val online: String,
-    val offline: String
-)
-
-public fun RBuilder.ringUserCard(user: UserCardModel, handler: RHandler<UserCardProps> = {}) {
-    UserCardModule.UserCard {
-        attrs.user = user
-        handler()
-    }
+public external interface UserCardWording {
+    public val banned: String
+    public val online: String
+    public val offline: String
 }
