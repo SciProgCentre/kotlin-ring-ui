e52edb7a6abb7c6226d1e01caac070d24b863d6e
Victor Turansky <victor.turansky@gmail.com>
2021-06-20 18:39:17 +0300

    Simplify `Island` declarations

--- src/main/kotlin/ringui/Island.kt
+++ src/main/kotlin/ringui/Island.kt
@@ -1,8 +1,20 @@
+@file:JsModule("@jetbrains/ring-ui/components/island/island")
+
 package ringui
 
 import react.RClass
-import react.RElementBuilder
 import react.dom.WithClassName
+
+// https://github.com/JetBrains/ring-ui/blob/master/components/island/island.js
+public external interface IslandProps : WithClassName {
+    public val narrow: Boolean
+    public val withoutPaddings: Boolean
+}
+
+@JsName("default")
+public external val Island: RClass<IslandProps>
+
+public external val AdaptiveIsland: RClass<IslandProps>
 
 // https://github.com/JetBrains/ring-ui/blob/master/components/island/header.js
 public external interface IslandHeaderProps : WithClassName {
@@ -10,6 +22,8 @@ public external interface IslandHeaderProps : WithClassName {
     public var wrapWithTitle: Boolean
     public var phase: Number
 }
+
+public external val Header: RClass<IslandHeaderProps>
 
 // https://github.com/JetBrains/ring-ui/blob/master/components/island/content.js
 public external interface IslandContentProps : WithClassName {
@@ -20,29 +34,4 @@ public external interface IslandContentProps : WithClassName {
     public var onScrollToBottom: () -> Unit
 }
 
-@JsModule("@jetbrains/ring-ui/components/island/island")
-@PublishedApi
-internal external object IslandModule {
-    @JsName("default")
-    val Island: RClass<IslandProps>
-    val Content: RClass<IslandContentProps>
-    val Header: RClass<IslandHeaderProps>
-    val AdaptiveIsland: RClass<IslandProps>
-}
-
-// https://github.com/JetBrains/ring-ui/blob/master/components/island/island.js
-public external interface IslandProps : WithClassName {
-    public val narrow: Boolean
-    public val withoutPaddings: Boolean
-}
-
-public inline val Island: RClass<IslandProps> get() = IslandModule.Island
-
-public inline val AdaptiveIsland: RClass<IslandProps> get() = IslandModule.AdaptiveIsland
-
-
-public inline val RElementBuilder<IslandProps>.Header: RClass<IslandHeaderProps>
-    get() = IslandModule.Header
-
-public inline val RElementBuilder<IslandProps>.Content: RClass<IslandContentProps>
-    get() = IslandModule.Content
+public external val Content: RClass<IslandContentProps>
