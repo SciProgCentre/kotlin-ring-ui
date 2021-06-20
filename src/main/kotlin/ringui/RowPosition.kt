22005f25e6d8f081ac0751e19eb0a8af6a5963f5
Victor Turansky <victor.turansky@gmail.com>
2021-06-20 18:16:34 +0300

    `RowPosition` - union

--- src/main/kotlin/ringui/RowPosition.kt
+++ src/main/kotlin/ringui/RowPosition.kt
@@ -1,8 +1,21 @@
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
 package ringui
 
-public enum class RowPosition {
-    xs,
-    sm,
-    md,
-    lg
+public sealed external interface RowPosition {
+    public companion object
 }
+
+public inline val RowPosition.Companion.xs: RowPosition
+    get() = "xs".unsafeCast<RowPosition>()
+
+public inline val RowPosition.Companion.sm: RowPosition
+    get() = "sm".unsafeCast<RowPosition>()
+
+public inline val RowPosition.Companion.md: RowPosition
+    get() = "md".unsafeCast<RowPosition>()
+
+public inline val RowPosition.Companion.lg: RowPosition
+    get() = "lg".unsafeCast<RowPosition>()
